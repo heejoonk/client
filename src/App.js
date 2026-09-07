58feaf5e256039407cba2e8637eb3f53b018d375
heejoonk <heejoonk5@naver.com>
2026-09-07 13:25:28 +0900

    sixth commit

--- src/App.js
+++ src/App.js
@@ -128,7 +128,7 @@ function App() {
 
     return (
         <div className="container">
-            <h2>AI CUSTOM SERVICE</h2>
+            <h2>AI FULL CUSTOM SERVICE</h2>
             <div>
                 <h3>Chatting List</h3>
                 <div>
